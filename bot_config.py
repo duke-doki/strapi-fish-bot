5c1e70cd8434918342095628dc72eea1ff849b73
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-07-16 12:36:49 +0300

    fix env

--- bot_config.py
+++ bot_config.py
@@ -1,9 +1,11 @@
 from environs import Env
 
 
-env = Env()
-env.read_env()
-starapi_token = env.str('API_TOKEN')
-host = env.str('HOST', 'localhost')
-port = env.str('PORT', '1337')
-headers = {'Authorization': f'bearer {starapi_token}'}
+def get_config():
+    env = Env()
+    env.read_env()
+    starapi_token = env.str('API_TOKEN')
+    host = env.str('HOST', 'localhost')
+    port = env.str('PORT', '1337')
+    headers = {'Authorization': f'bearer {starapi_token}'}
+    return host, port, headers
--- strapi_fetcher.py
+++ strapi_fetcher.py
@@ -3,13 +3,15 @@ from io import BytesIO
 
 import requests
 
-from bot_config import host, port, headers
+from bot_config import get_config
+
+HOST, PORT, HEADERS = get_config()
 
 
 def fetch_products():
-    url = f'http://{host}:{port}/api/products'
+    url = f'http://{HOST}:{PORT}/api/products'
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
 
     products = response.json()
@@ -17,22 +19,22 @@ def fetch_products():
 
 
 def get_product_by_id(product_id):
-    url = f'http://{host}:{port}/api/products/{product_id}'
+    url = f'http://{HOST}:{PORT}/api/products/{product_id}'
     params = {'populate': 'Picture'}
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=HEADERS, params=params)
     response.raise_for_status()
 
     product = response.json()
     image_url = \
     product['data']['attributes']['Picture']['data'][0]['attributes']['url']
-    image_url = f'http://{host}:{port}{image_url}'
+    image_url = f'http://{HOST}:{PORT}{image_url}'
     image = download_image(image_url, product_id)
     return product, image
 
 
 def download_image(url, pic_id):
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
 
     image_data = BytesIO(response.content)
@@ -40,13 +42,13 @@ def download_image(url, pic_id):
 
 
 def create_or_update_cart(chat_id, products: dict):
-    url = f'http://{host}:{port}/api/carts'
+    url = f'http://{HOST}:{PORT}/api/carts'
     params = {'filters[chat_id][$eq]': chat_id, 'populate': '*'}
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=HEADERS, params=params)
     response.raise_for_status()
     response_cart = response.json()
 
-    headers['Content-Type'] = 'application/json'
+    HEADERS['Content-Type'] = 'application/json'
     product_carts_ids = []
     for product_id, product_quantity in products.items():
         product_cart = create_product_cart(product_id, product_quantity)
@@ -67,7 +69,7 @@ def create_or_update_cart(chat_id, products: dict):
         }
         response = requests.post(
             url,
-            headers=headers,
+            headers=HEADERS,
             data=json.dumps(data)
         )
     else:
@@ -80,7 +82,7 @@ def create_or_update_cart(chat_id, products: dict):
         url = f'{url}/{cart_id}'
         response = requests.put(
             url,
-            headers=headers,
+            headers=HEADERS,
             data=json.dumps(data)
         )
     response.raise_for_status()
@@ -89,8 +91,8 @@ def create_or_update_cart(chat_id, products: dict):
 
 
 def create_product_cart(product_id, quantity=1):
-    url = f'http://{host}:{port}/api/cart-products'
-    headers['Content-Type'] = 'application/json'
+    url = f'http://{HOST}:{PORT}/api/cart-products'
+    HEADERS['Content-Type'] = 'application/json'
     data = {
         'data': {
             'product': product_id,
@@ -99,7 +101,7 @@ def create_product_cart(product_id, quantity=1):
     }
     response = requests.post(
         url,
-        headers=headers,
+        headers=HEADERS,
         data=json.dumps(data)
     )
     response.raise_for_status()
@@ -108,9 +110,9 @@ def create_product_cart(product_id, quantity=1):
 
 
 def get_cart_products_by_id(chat_id):
-    url = f'http://{host}:{port}/api/carts'
+    url = f'http://{HOST}:{PORT}/api/carts'
     params = {'filters[chat_id][$eq]': chat_id, 'populate': '*'}
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=HEADERS, params=params)
     response.raise_for_status()
 
     cart = response.json()
@@ -136,9 +138,9 @@ def get_cart_products_by_id(chat_id):
 
 
 def get_cart_product_by_id(cart_product_id):
-    url = f'http://{host}:{port}/api/cart-products/{cart_product_id}'
+    url = f'http://{HOST}:{PORT}/api/cart-products/{cart_product_id}'
     params = {'populate': '*'}
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=HEADERS, params=params)
     response.raise_for_status()
 
     cart_product = response.json()
@@ -146,8 +148,8 @@ def get_cart_product_by_id(cart_product_id):
 
 
 def delete_cart_product(cart_product_id):
-    url = f'http://{host}:{port}/api/cart-products/{cart_product_id}'
-    response = requests.delete(url, headers=headers)
+    url = f'http://{HOST}:{PORT}/api/cart-products/{cart_product_id}'
+    response = requests.delete(url, headers=HEADERS)
     response.raise_for_status()
 
     cart_product = response.json()
@@ -155,14 +157,14 @@ def delete_cart_product(cart_product_id):
 
 
 def add_email_to_cart(chat_id, email):
-    url = f'http://{host}:{port}/api/carts'
+    url = f'http://{HOST}:{PORT}/api/carts'
     params = {'filters[chat_id][$eq]': chat_id, 'populate': '*'}
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=HEADERS, params=params)
     response.raise_for_status()
     response_cart = response.json()
 
     cart_id = response_cart['data'][0]['id']
-    headers['Content-Type'] = 'application/json'
+    HEADERS['Content-Type'] = 'application/json'
     data = {
         'data': {
             'email': email
@@ -171,7 +173,7 @@ def add_email_to_cart(chat_id, email):
     url = f'{url}/{cart_id}'
     response = requests.put(
         url,
-        headers=headers,
+        headers=HEADERS,
         data=json.dumps(data)
     )
     response.raise_for_status()
@@ -183,9 +185,9 @@ def add_email_to_cart(chat_id, email):
 
 
 def get_email_by_id(chat_id):
-    url = f'http://{host}:{port}/api/carts'
+    url = f'http://{HOST}:{PORT}/api/carts'
     params = {'filters[chat_id][$eq]': chat_id, 'populate': '*'}
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=HEADERS, params=params)
     response.raise_for_status()
 
     cart = response.json()
